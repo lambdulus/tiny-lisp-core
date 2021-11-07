8a8704828b0e44cd48a1500de551759f1e71cf1e
Vojtech Rozhon <rozhovoj@fit.cvut.cz>
2021-11-07 12:45:43 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import {Parser} from "./parser/Parser";
-import {Interpreter} from "./interpreter/Interpreter";
-import {SECDArray} from "./parser/SECDArray";
+
+import {Parser} from "./src/parser/Parser";
+import {Interpreter} from "./src/interpreter/Interpreter";
+import {SECDArray} from "./src/utility/SECD/SECDArray"
+import { SECDVisitor } from "./src/utility/visitors/SECDVisitor";
+import { SECDValue } from "./src/utility/SECD/SECDValue";
+import { ColourType } from "./src/utility/SECD/ColourType";
 
 
-export {Parser, Interpreter, SECDArray}
+export {Parser, Interpreter, SECDArray, SECDVisitor, SECDValue, ColourType}
 
 let parser = new Parser();
-
+/*
 
 console.log(parser.parse("(- 10 ( + 2 ( * 4 5)))"));
 let interpreter = new Interpreter(parser.parse("(- 10 ( + 2 ( * 4 5)))"));
@@ -22,7 +26,7 @@ interpreter.detectAction();
 
 console.log(parser.parse("(+ 1 ((lambda (x y) (+ x y)) 10 20))"));
 interpreter = new Interpreter(parser.parse("(+ 1 ((lambda (x y) (+ x y)) 10 20))"));
-interpreter.detectAction();
+interpreter.detectAction();*/
 
 console.log(parser.parse("(letrec((fact) " +
                                     "((lambda(n)" +
@@ -30,22 +34,22 @@ console.log(parser.parse("(letrec((fact) " +
                                             "1" +
                                             "(* n (fact (- n 1)))))))" +
                                 "(fact 2))"));
-interpreter = new Interpreter(parser.parse("(letrec((fact) " +
+let interpreter = new Interpreter(parser.parse("(letrec((fact) " +
                                                             "((lambda(n)" +
                                                                 "(if (= n 0)" +
                                                                     "1" +
                                                                     "(* n (fact (- n 1)))))))" +
                                                         "(fact 2))"));
 interpreter.detectAction();
-
-console.log(parser.parse("'(1 2 3)"));
 /*
+console.log(parser.parse("'(1 2 3)"));
+
 console.log(parser.parse("(define cadr(lst)" +
                                         "(car (cdr lst)))" +
                                 "(define cdadr(lst)" +
                                         "(cdr (cadr lst)))" +
                                 "(cdadr '(1 2 3))"));
-let interpreter = new Interpreter(parser.parse(
+interpreter = new Interpreter(parser.parse(
                             "(define cadr(lst)" +
                                         "(car (cdr lst)))" +
                                 "(define cdadr(lst)" +
